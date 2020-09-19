b0c01d1232c17bacc2e2281b2f4953e335e5983c
Eran Agmon <agmon.eran@gmail.com>
2020-09-19 16:50:25 -0700

    add info option

--- scripts/access_db.py
+++ scripts/access_db.py
@@ -15,39 +15,45 @@ class AccessDB(object):
         self.client = MongoClient(url)
         self.db = getattr(self.client, database)
 
+    def print_info(self, experiment_id):
+        data = self.db.configuration.find(
+            {'experiment_id': experiment_id},
+            {'name': 1, 'description': 1, 'time_created': 1})
+        name = data[0]['name']
+        description = data[0]['description']
+        time_created = data[0]['time_created']
+        print(
+            'id: {}\n '
+            'experiment name: {}\n '
+            'time_created: {}\n'
+            'description: {}\n'.format(
+                experiment_id,
+                name,
+                time_created,
+                description)
+        )
+
     def access(self):
 
         if self.args.list:
             experiment_ids = self.db.configuration.distinct('experiment_id')
             print('experiment ids: {}'.format(experiment_ids))
 
-        if self.args.view:
-            experiment_ids = self.db.configuration.distinct('experiment_id')
-            for exp_id in experiment_ids:
-                data = self.db.configuration.find(
-                    {'experiment_id': exp_id},
-                    {'name': 1, 'description': 1, 'time_created': 1})
-                name = data[0]['name']
-                description = data[0]['description']
-                time_created = data[0]['time_created']
-                print(
-                    'experiment name: {}\n '
-                    'id: {}\n '
-                    'time_created: {}\n'
-                    'description: {}\n'.format(
-                        name,
-                        exp_id,
-                        time_created,
-                        description)
-                )
+        if self.args.info:
+            for exp_id in self.args.info:
+                self.print_info(exp_id)
 
-
-        # delete database entries
         if self.args.delete:
             for delete in self.args.delete:
                 query = {'experiment_id': delete}
                 self.db.history.delete_many(query)
                 self.db.configuration.delete_many(query)
+
+        if self.args.view:
+            experiment_ids = self.db.configuration.distinct('experiment_id')
+            for exp_id in experiment_ids:
+                self.print_info(exp_id)
+
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -55,7 +61,14 @@ class AccessDB(object):
             nargs='+',
             type=str,
             default='None',
-            help='list of experiment ids to delete')
+            help='provide list of experiment ids to delete')
+
+        parser.add_argument(
+            '-i', '--info',
+            nargs='+',
+            type=str,
+            default=False,
+            help='get info on a list of experiment ids')
 
         parser.add_argument(
             '-l', '--list',
@@ -67,7 +80,7 @@ class AccessDB(object):
             '-v', '--view',
             action='store_true',
             default=False,
-            help='show all experiment in db with description')
+            help='get all experiment ids in db')
 
         return parser
 
